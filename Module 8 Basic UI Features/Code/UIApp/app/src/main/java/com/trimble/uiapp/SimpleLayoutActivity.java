1bff18f112f8290db3540ea7286c950a2c1786f2
Jeffrey McBride <jeffrey.mcbride@gmail.com>
2014-07-16 07:20:46 -0400

    Added links for SQLite tools

---
+++ Module 8 Basic UI Features/Code/UIApp/app/src/main/java/com/trimble/uiapp/SimpleLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.trimble.uiapp;
+
+
+import android.os.Bundle;
+
+public class SimpleLayoutActivity extends MenuActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_layout);
+        //this.getActionBar().setTitle("Simple Layout");
+        this.getActionBar().hide();
+    }
+}
